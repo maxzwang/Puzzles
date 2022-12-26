af3005aa1fc6d0d382e29c0d7435497eb5605f31
Maxwell Wang <max.z.wang@gmail.com>
2022-12-26 15:47:44 -0500

    day 3, actually pretty proud of my code for once :')

--- AoC_2022/3b.py
+++ AoC_2022/3b.py
@@ -1,23 +1,24 @@
+import string
+
 input = []
 
 with open('3_input.txt', 'r') as file:
     for line in file:
         input.append(line.rstrip())
 
-LETTERS = [letter for letter in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']
+LETTERS = list(string.ascii_lowercase + string.ascii_uppercase)
 VALUES = [value for value in range(1, 52+1)]
 
 PRIORITY_DICT = {LETTERS[i]:VALUES[i] for i in range(52)}
 
 total = 0
 
-for sack in input:
-    halfsize = int(len(sack) / 2)
-    
-    left = sack[:halfsize]
-    right = sack[halfsize:]
+for i in range(int(len(input)/3)):
+    sack1 = input[3*i]
+    sack2 = input[3*i + 1]
+    sack3 = input[3*i + 2]
 
-    common_item = (set(left) & set(right)).pop()
+    common_item = (set(sack1) & set(sack2) & set(sack3)).pop()
 
     total += PRIORITY_DICT[common_item]
 
